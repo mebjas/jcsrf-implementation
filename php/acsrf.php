bc2f9cfe8dd7dae0796a84e96bfa704abfc13fad
minhaz <minhazav@gmail.com>
2014-03-05 13:03:57 +0530

    library updated

--- php/acsrf.php
+++ php/acsrf.php
@@ -4,24 +4,8 @@
  * inspire form OWASP
  * Uses token based mitigation techniques for CSRF prevention!
  */
-
-/**
- * if CSRFtoken does not exists unset all existing POST entries 
- */
-if( count($_POST) && !isset($_POST['CSRFtoken']))
-{
-	//@todo: log the request data to db
-	unset($_POST);
-}
-
-/**
- * if CSRFtoken does not match unset all POST entries
- */
-if( isset($_POST['CSRFtoken']) && isset($_COOKIE['CSRFtoken']) && ( $_POST['CSRFtoken'] != $_COOKIE['CSRFtoken'] ) )
-{
-	//@todo: log th request and alert the user
-	unset($_POST);
-}
+var_dump($_POST);
+define("cookie_name","CSRF_AUTH_TOKEN");
 
 
 /** 
@@ -32,19 +16,32 @@ class acsrf
 	//list of whitelisted website 
 	private $whitelist = array();
 
+	//autherisation key
+	private $key;
 
+	/**
+	 * constructor
+	 */
+	function __construct($key = null)
+	{
+		if($key == null)
+		{
+			$this->generateKey();
+		}
+		else $this->key = $key;
+	}
 	/**
 	 * function to generate pseudo random code
 	 */
-	public static function getKey($unique_form_name)
+	public function generateKey()
 	{
 		if (function_exists("hash_algos") and in_array("sha512",hash_algos()))
 		{
-			$token  =hash("sha512",uniqid() .mt_rand(0,mt_getrandmax()));
+			$this->key = hash("sha512",uniqid() .mt_rand(0,mt_getrandmax()));
 		}
 		else
 		{
-			$token = ' ';
+			$this->key = ' ';
 			for ($i = 0; $i < 128; ++$i)
 			{
 				$r = mt_rand(0,35);
@@ -56,10 +53,25 @@ class acsrf
 				{ 
 					$c = chr(ord('0')+$r-26);
 				} 
-				$token .= $c;
+				$this->key .= $c;
 			}
 		}
-		return $token;
+	}
+
+	/**
+	 * function to get key
+	 */
+	public function getKey()
+	{
+		return $this->key;
+	}
+
+	/**
+	 * function to get key
+	 */
+	public function setKeyToCookie()
+	{
+		setcookie(cookie_name,$this->key,time()+1000);
 	}
 
 	/**
@@ -77,6 +89,74 @@ class acsrf
 	{
 		return explode("_",convert_uudecode($hash));
 	}
+
+	/**
+	 * function to print script to page
+	 */
+	public function injectScript()
+	{
+		echo "<script>";
+		echo "
+		var forms = document.getElementsByTagName('form');
+		/*
+		for(i=0;i<forms.length;i++)
+		{
+			forms[i].addEventListener('submit', function(e) {
+				var obj = e.srcElement ? e.srcElement : e.target;
+				console.log(obj);
+				obj.innerHTML += '<input type=\"hidden\" name=\"" .cookie_name."\" id=\"" .cookie_name."\" value=\"'+getCookie('".cookie_name."')+'\">';
+				obj.submit();
+        		e.preventDefault();
+   				return false;
+			}, false);
+		}
+		*/
+		function getCookie(cname)
+		{
+		var name = cname + \"=\";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) 
+		  {
+		  var c = ca[i].trim();
+		  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+		}
+		return \"\";
+		}
+		for(i=0;i<forms.length;i++)
+		{
+			forms[i].innerHTML += '<input type=\"hidden\" name=\"" .cookie_name."\" id=\"" .cookie_name."\" value=\"'+getCookie('".cookie_name."')+'\">';
+		}		
+
+
+		";
+		echo "</script>";
+	}
+
 }
 
-
+/**
+ * if CSRFtoken does not exists unset all existing POST entries 
+ */
+if(!isset($_POST[cookie_name]))
+{
+	//@todo: log the request data to db
+	foreach($_POST as $key => $values)
+		unset($_POST[$key]);
+	$csrfguardObj = new acsrf();
+	$csrfguardObj->setKeyToCookie();
+}
+else if(( isset($_POST[cookie_name]) && 
+	isset($_COOKIE[cookie_name]) ) && 
+	( $_POST[cookie_name] != $_COOKIE[cookie_name] ))
+{
+	//@todo: log the request data to db
+	foreach($_POST as $key => $values)
+		unset($_POST[$key]);
+	$csrfguardObj = new acsrf();
+	$csrfguardObj->setKeyToCookie();
+}
+else
+{
+	$csrfguardObj = new acsrf($_COOKIE[cookie_name]);
+	$csrfguardObj->setKeyToCookie();
+}
